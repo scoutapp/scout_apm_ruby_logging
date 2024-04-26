fd3423d46ec5223a730cfd5c8d3b263ab7edeb9f
Jack Rothrock <jack@scoutapm.com>
2024-04-26 11:45:51 -0600

    Have daemon cleanup PID file at exit

--- lib/scout_apm/logging/monitor/monitor.rb
+++ lib/scout_apm/logging/monitor/monitor.rb
@@ -8,9 +8,19 @@ module ScoutApm
   module Logging
     class Monitor
       def run
+        add_exit_handler
+
         loop do
           sleep 1
           puts "Running..."
+        end
+      end
+
+      private
+
+      def add_exit_handler
+        at_exit do
+          File.delete(ScoutApm::Logging::MonitorManager::PID_FILE)
         end
       end
     end
