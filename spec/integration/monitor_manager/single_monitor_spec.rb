4904d992c86698a797b9fa521c99c03d0ff05bb2
Jack Rothrock <jack@scoutapm.com>
2024-06-17 13:28:52 -0600

    Fix lint

--- spec/integration/monitor_manager/single_monitor_spec.rb
+++ spec/integration/monitor_manager/single_monitor_spec.rb
@@ -1,13 +1,11 @@
 # This is really meant to simulate multiple processes calling the MonitorManager setup.
-# Trying to create **multiple fake** rails environments is a bit challening 
+# Trying to create **multiple fake** rails environments is a bit challening
 # (such as a rails app and a couple rails runners).
 
 # See: https://github.com/rails/rails/blob/6d126e03dbbf5d30fa97b580f7dee46343537b7b/railties/test/isolation/abstract_unit.rb#L339
 
 # We simulate the ultimate outcome here -- ie Railtie invoking the setup.
 require 'spec_helper'
-
-require 'pry'
 
 describe ScoutApm::Logging do
   it 'Should only create a single monitor daemon if manager is called multiple times' do
