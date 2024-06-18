b941b52daf6bedca20a1725c12ca8393be99d6e3
Jack Rothrock <jack@scoutapm.com>
2024-06-17 18:31:27 -0600

    Add ability to configure the collector

--- spec/unit/monitor/collector/configuration_spec.rb
+++ spec/unit/monitor/collector/configuration_spec.rb
@@ -7,6 +7,51 @@ require_relative '../../../../lib/scout_apm/logging/monitor/collector/configurat
 describe ScoutApm::Logging::Collector::Configuration do
   it 'creates the correct configuration for the otelcol' do
     context = ScoutApm::Logging::Context.new
+    setup_collector_config!(context)
+
+    expect(File.exist?(context.config.value('collector_config_file'))).to be_truthy
+    config = YAML.load_file(context.config.value('collector_config_file'))
+
+    expect(config['exporters']['otlp']['endpoint']).to eq('https://otlp.telemetryhub.com:4317')
+    expect(config['exporters']['otlp']['headers']['x-telemetryhub-key']).to eq('00001000010000abc')
+    expect(config['receivers']['filelog']['include']).to eq(['/tmp/fake_log_file.log'])
+  end
+
+  it 'merges in a logs config correctly' do
+    ENV['SCOUT_LOGS_CONFIG'] = File.expand_path('../../../data/logs_config.yml', __dir__)
+
+    context = ScoutApm::Logging::Context.new
+    setup_collector_config!(context)
+
+    expect(File.exist?(context.config.value('collector_config_file'))).to be_truthy
+    config = YAML.load_file(context.config.value('collector_config_file'))
+
+    expect(config['exporters']['otlp']['endpoint']).to eq('https://otlp.telemetryhub.com:4317')
+    expect(config['exporters']['otlp']['headers']['x-telemetryhub-key']).to eq('00001000010000abc')
+    expect(config['receivers']['filelog']['include']).to eq(['/tmp/fake_log_file.log'])
+
+    # Verify merge and consistent keys
+    expect(config['extensions']['file_storage/otc']['directory']).to eq('/dev/null')
+    expect(config['extensions']['file_storage/otc']['timeout']).to eq('10s')
+  end
+
+  it 'handles an empty logs config file well' do
+    ENV['SCOUT_LOGS_CONFIG'] = File.expand_path('../../../data/empty_logs_config.yml', __dir__)
+
+    context = ScoutApm::Logging::Context.new
+    setup_collector_config!(context)
+
+    expect(File.exist?(context.config.value('collector_config_file'))).to be_truthy
+    config = YAML.load_file(context.config.value('collector_config_file'))
+
+    expect(config['exporters']['otlp']['endpoint']).to eq('https://otlp.telemetryhub.com:4317')
+    expect(config['exporters']['otlp']['headers']['x-telemetryhub-key']).to eq('00001000010000abc')
+    expect(config['receivers']['filelog']['include']).to eq(['/tmp/fake_log_file.log'])
+  end
+
+  private
+
+  def setup_collector_config!(context)
     conf_file = File.expand_path('../../../data/mock_config.yml', __dir__)
     context.config = ScoutApm::Logging::Config.with_file(context, conf_file)
     context.health_check_port = 1234
@@ -15,12 +60,5 @@ describe ScoutApm::Logging::Collector::Configuration do
 
     collector_config = ScoutApm::Logging::Collector::Configuration.new(context)
     collector_config.setup!
-
-    expect(File.exist?(context.config.value('collector_config_file'))).to be_truthy
-    config = YAML.load_file(context.config.value('collector_config_file'))
-
-    expect(config['exporters']['otlp']['endpoint']).to eq('https://otlp.telemetryhub.com:4317')
-    expect(config['exporters']['otlp']['headers']['x-telemetryhub-key']).to eq('00001000010000abc')
-    expect(config['receivers']['filelog']['include']).to eq(['/tmp/fake_log_file.log'])
   end
 end
