b49c0be3ca880fe1f0303eb24150c42de543b5e6
Jack Rothrock <jack@scoutapm.com>
2024-06-25 11:11:04 -0600

    Improve code consistency

--- lib/scout_apm/logging/state.rb
+++ lib/scout_apm/logging/state.rb
@@ -4,23 +4,29 @@ module ScoutApm
   module Logging
     # Responsibling for ensuring safe interprocess persitance around configuration state.
     class Config::State
-      def self.load_state_from_file(config)
-        return unless File.exist?(config.value('monitor_state_file'))
+      attr_reader :context
 
-        file_contents = File.read(config.value('monitor_state_file'))
+      def initialize(context)
+        @context = context
+      end
+
+      def load_state_from_file
+        return unless File.exist?(context.config.value('monitor_state_file'))
+
+        file_contents = File.read(context.config.value('monitor_state_file'))
         data = JSON.parse(file_contents)
       end
 
-      def self.flush_to_file!(config)
-        Utils.ensure_directory_exists(config.value('monitor_state_file'))
+      def flush_to_file!
+        Utils.ensure_directory_exists(context.config.value('monitor_state_file'))
 
-        File.open(config.value('monitor_state_file'), (File::RDWR | File::CREAT), 0644) do |file|
+        File.open(context.config.value('monitor_state_file'), (File::RDWR | File::CREAT), 0644) do |file|
           file.flock(File::LOCK_EX)
 
           # TODO: We will need to merge monitored_logs based on the data in the state file, and not
           # what is currently in the config.
           data = Config::ConfigState.get_values_to_set.inject({}) do |memo, key|
-            memo[key] = config.value(key)
+            memo[key] = context.config.value(key)
             memo
           end
 
