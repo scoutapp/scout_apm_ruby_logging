fa29f560af364e8d2bf2268a0ff1d503d272b6f2
Jack Rothrock <jack@scoutapm.com>
2024-09-12 15:49:36 -0600

    Improve tests

--- spec/integration/rails/lifecycle_spec.rb
+++ spec/integration/rails/lifecycle_spec.rb
@@ -1,9 +1,33 @@
+require 'webmock/rspec'
+
 require 'spec_helper'
+require 'zlib'
+require 'stringio'
 require_relative '../../rails/app'
+require_relative '../../../lib/scout_apm/logging/loggers/opentelemetry/exporter/proto/collector/logs/v1/logs_service_pb'
 
 describe ScoutApm::Logging do
+  before do
+    # Allow real HTTP requests to go through
+    WebMock.allow_net_connect!
+
+    @file_path = '/app/response_body.txt'
+    # Capture the outgoing HTTP request to inspect its values
+    stub_request(:post, 'https://otlp-devel.scoutotel.com:4318/v1/logs')
+      .to_return do |request|
+        # We have to write to the file, as we are applying the patch in the fork, and
+        # need to run the expectations in the main process.
+        File.open(@file_path, 'a+') do |file|
+          msgs = transform_body_to_msgs(request.body)
+          file.puts(msgs)
+        end
+        { body: '', headers: {}, status: 200 }
+      end
+  end
+
   it 'checks the Rails lifecycle for creating the daemon and collector processes' do
     ENV['SCOUT_LOGS_MONITOR'] = 'true'
+    ENV['SCOUT_LOGS_REPORTING_ENDPOINT_HTTP'] = 'https://otlp-devel.scoutotel.com:4318/v1/logs'
 
     context = ScoutApm::Logging::Context.new
     context.config = ScoutApm::Logging::Config.with_file(context, context.config.value('config_file'))
@@ -17,6 +41,8 @@ describe ScoutApm::Logging do
 
     # Call the app to generate the logs
     `curl localhost:8080`
+
+    sleep 5
 
     proxy_dir = context.config.value('logs_proxy_log_dir')
     files = Dir.entries(proxy_dir) - ['.', '..']
@@ -32,20 +58,45 @@ describe ScoutApm::Logging do
       end
     end
 
-    messages = lines.map { |item| item['msg'] }
+    local_messages = lines.map { |item| item['msg'] }
 
-    # Verify we have all the logs
-    expect(messages.count('[TEST] Some log')).to eq(1)
-    expect(messages.count('[YIELD] Yield Test')).to eq(1)
-    expect(messages.count('Another Log')).to eq(1)
-    expect(messages.count('Should not be captured')).to eq(0)
+    # Verify we have all the logs in the local log file
+    expect(local_messages.count('[TEST] Some log')).to eq(1)
+    expect(local_messages.count('[YIELD] Yield Test')).to eq(1)
+    expect(local_messages.count('Another Log')).to eq(1)
+    expect(local_messages.count('Should not be captured')).to eq(0)
 
     log_locations = lines.map { |item| item['log_location'] }.compact
 
     # Verify that log attributes aren't persisted
     expect(log_locations.size).to eq(1)
 
+    # Verify the logs are sent to the receiver
+    receiver_contents = File.readlines(@file_path, chomp: true)
+    expect(receiver_contents.count('[TEST] Some log')).to eq(1)
+    expect(receiver_contents.count('[YIELD] Yield Test')).to eq(1)
+    expect(receiver_contents.count('Another Log')).to eq(1)
+    expect(receiver_contents.count('Should not be captured')).to eq(0)
+
     # Kill the rails process. We use kill as using any other signal throws a long log line.
     Process.kill('KILL', rails_pid)
+  end
+
+  private
+
+  def transform_body_to_msgs(body)
+    gz = Zlib::GzipReader.new(StringIO.new(body))
+    uncompressed = gz.read
+
+    value = ScoutApm::Logging::Loggers::Opentelemetry::Proto::Collector::Logs::V1::ExportLogsServiceRequest.decode(uncompressed)
+    value_hash = value.to_h
+
+    value_hash[:resource_logs].map do |item|
+      item[:scope_logs].map do |sl|
+        sl[:log_records].map do |log|
+          log[:body][:string_value]
+        end
+      end
+    end.flatten
   end
 end
